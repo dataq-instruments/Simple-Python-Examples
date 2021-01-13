5087b333b74912f1700325f8c27a09662edf73f8
Dataq Instruments <45690253+dataq-instruments@users.noreply.github.com>
2021-01-13 13:26:37 -0500

    Add files via upload

--- simpletest_binary.py
+++ simpletest_binary.py
@@ -5,15 +5,7 @@ import sys
 
 #PSerial 3.5 is in use for this example
 #
-#After installing Python, one needs to install following modules under command prompt
-#
-#    python -m pip install -U pyserial --user
-#    python -m pip install -U keyboard --user
-#
-#This sample program will start DATAQ Instrument (11xx/2108/4108/4208/4718) usb data acquisition products
-#Please make sure the device is in CDC mode (blinking yellow when conntected)
 #Press key 'x' to EXIT
-#since serDataq.readline waits until a line is received, low sample rate makes the program slow in response to key stroke
 
 CONST_SER_PORT = 'COM11'   #get the com port from device manger and enter it here
 
@@ -28,14 +20,14 @@ serDataq.write(b"slist 0 0\r")   #scan list position 0 channel 0 thru channel 7
 serDataq.write(b"slist 1 1\r")   #scan list position 0 channel 0 thru channel 7
 serDataq.write(b"slist 2 2\r")   #scan list position 0 channel 0 thru channel 7
 serDataq.write(b"srate 6000\r") 
-serDataq.write(b"ps 0\r") 
 serDataq.write(b"dec 100\r") 
 serDataq.write(b"deca 3\r")
+serDataq.write(b"ps 0\r")        #if you modify sample rate, you need modify this accordingly
 time.sleep(1)  
 serDataq.reset_input_buffer()
-serDataq.write(b"start\r")           #start scanning
+serDataq.write(b"start\r")       #start scanning
 
-numofchannel=3
+numofchannel=3 #if you modify the slist, you need modify this accordingly
 numofbyteperscan=2*numofchannel
 
 while True:
@@ -49,9 +41,8 @@ while True:
         else:
             i= serDataq.in_waiting
             if (i/numofbyteperscan)>0:
-                
+                #we always read in scans
                 response = serDataq.read(i - i%numofbyteperscan)
-                #we will just print out the first scan
 
                 Channel =[]
 
@@ -60,8 +51,8 @@ while True:
                     if adc>32767:
                         adc=adc-65536
                     Channel.append (adc)
-
-                #print ("{} {} {}".format(Channel[0], Channel[1], Channel[2]))
+    
+                #Print only the first scan for demo purpose
                 print ("%5d %5d %5d" %(Channel[0], Channel[1], Channel[2]))
             pass
     except:
