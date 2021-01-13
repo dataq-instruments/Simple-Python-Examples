75056d3d50812134735c0058892e60604ba1c67c
Dataq Instruments <45690253+dataq-instruments@users.noreply.github.com>
2021-01-13 13:28:13 -0500

    Add files via upload

--- simpletest_ascii.py
+++ simpletest_ascii.py
@@ -3,34 +3,12 @@ import keyboard
 import time
 import sys
 
-#After installing Python, one needs to install following modules under command prompt
-#
-#    python -m pip install -U pyserial --user
-#    python -m pip install -U keyboard --user
-#
-#This sample program will start DATAQ Instrument (11xx/2108/4108/4208/4718) usb data acquisition products
-#Please make sure the device is in CDC mode (blinking yellow when conntected)
 #Press key 'x' to EXIT
 #since serDataq.readline waits until a line is received, low sample rate makes the program slow in response to key stroke
 
 CONST_SER_PORT = 'COM11'   #get the com port from device manger and enter it here
 
 serDataq = serial.Serial(CONST_SER_PORT)
-
-#for Windows 10, one may run into the exception of "serial.serialutil.SerialException: Cannot configure port, something went wrong. Original message: WindowsError(87, 'Incorrect Parameter.') exception."
-#
-#Work around until pyserial has fix:, find serialwin32.py inside the folder of c:\users\??? window 10\AppData\Roaming\Python\Python37\site-packages\serial\
-#then comment out the following lines in line 219:
-#
-#    if not win32.SetCommState(self._port_handle, ctypes.byref(comDCB)):
-#    raise SerialException(
-#        'Cannot configure port, something went wrong. '
-#        'Original message: {!r}'.format(ctypes.WinError()))
-#
-#
-#To single step through PY codes, follow
-#https://stackoverflow.com/questions/4929251/how-to-step-through-python-code-to-help-debug-issues
-
 
 serDataq.write(b"stop\r")        #stop in case device was left scanning
 serDataq.write(b"eol 1\r") 
